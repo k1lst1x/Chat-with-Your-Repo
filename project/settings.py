936f91bddf05a8b3cb49bce49900fa7a6541a8f6
saint <damir.dzhubaliev@gmail.com>
2025-03-28 20:45:26 +0500

    small settings.py update

--- project/settings.py
+++ project/settings.py
@@ -135,7 +135,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-STATIC_ROOT = "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 MEDIA_URL = '/uploads/'
